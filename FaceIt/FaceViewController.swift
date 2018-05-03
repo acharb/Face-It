bf7330e59948a60a394970853b7eb28deae73522
charb <30449853+aleccharb@users.noreply.github.com>
2018-05-02 22:00:33 -0400

    Initial commit

---
+++ FaceIt/FaceViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  FaceIt
+//
+//  Created by Alec Charbonneau on 12/31/17.
+//  Copyright © 2017 Alec Charbonneau. All rights reserved.
+//
+
+import UIKit
+
+class FaceViewController: UIViewController {
+    
+    
+
+
+}
+
